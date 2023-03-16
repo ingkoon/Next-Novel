70c6a3afa5d04c6aef01de38b7384613309f2864
subinKim <subin3277@gmail.com>
2023-03-16 17:32:30 +0900

    fix: css수정

--- Next-Novel/src/components/NewBookList.jsx
+++ Next-Novel/src/components/NewBookList.jsx
@@ -5,7 +5,13 @@ export default function NewBookList() {
   return (
     <div className='newbooklist'>
       <img src={process.env.PUBLIC_URL+'/img/circles_left.svg'} className='circle_left' ></img>
+      <div className='newbook_before'>
+        &lt;
+      </div>
       <NewCard/>
+      <div className='newbook_next'>
+        &gt;
+      </div>
       <img src={process.env.PUBLIC_URL+'/img/circles_right.svg'} className='circle_right' ></img>
     </div>
   )
