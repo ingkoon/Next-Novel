115d65382e9e567db3e2f29912c3d8a962c76b61
소지현 <s688922@gmail.com>
2023-03-20 12:30:23 +0900

    style : NovelWrite step0 시작

--- Next-Novel/src/components/BeforeStartWrite.jsx
+++ Next-Novel/src/components/BeforeStartWrite.jsx
@@ -1,12 +1,32 @@
 import React from "react";
+import TitleBar from "./TitleBar";
+import "./BeforeStartWrite.css";
 
 export default function BeforeStartWrite({ step, setStep }) {
   return (
     <div>
-      <h1>작업실 컴포넌트</h1>
-      <div>
-        <h2>내가 그리고 AI가 써주는 소설을 만들어볼까요?</h2>
-        <button onClick={() => setStep(1)}>시작하기</button>
+      <TitleBar
+        name="작업실"
+        intro="디테일이 생명입니다."
+        subintro1="work-shop"
+        subintro2="stajio"
+        img="pen"
+      />
+      <div className="before-start-write">
+        <img
+          src={process.env.PUBLIC_URL + "/img/circles_left.svg"}
+          className="circle_left"
+          alt="circle_left"
+        ></img>
+        <div className="before-start-write-box">
+          <span>내가 그리고 AI가 써주는 소설을 만들어볼까요?</span>
+          <button onClick={() => setStep(1)}>시작하기</button>
+        </div>
+        <img
+          src={process.env.PUBLIC_URL + "/img/circles_right.svg"}
+          className="circle_right"
+          alt="circle_right"
+        ></img>
       </div>
     </div>
   );
