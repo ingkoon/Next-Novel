b8bea8f1c5e30bcdd35db57658c5c4265a8ed3ad
서철원 <96charles@naver.com>
2023-03-20 16:55:47 +0900

    style : h1 tag  change

--- Next-Novel/src/pages/NovelIntro.jsx
+++ Next-Novel/src/pages/NovelIntro.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import TitleBar from '../components/TitleBar'
 
 export default function NovelIntro() {
   return (
     <>
-      <h1>NovelIntro</h1>
+      <h1>NovelIntro : 책의 프리뷰를 보는 곳입니다(조회수, 좋아요, 감상평)</h1>
     </>
   );
 }
