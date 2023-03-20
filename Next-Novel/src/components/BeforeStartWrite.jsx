cc5f1fad361a4ba9ea7605238276d696688ec815
소지현 <s688922@gmail.com>
2023-03-20 14:35:25 +0900

    style : NovelWrite step0 배치중

--- Next-Novel/src/components/BeforeStartWrite.jsx
+++ Next-Novel/src/components/BeforeStartWrite.jsx
@@ -4,7 +4,7 @@ import "./BeforeStartWrite.css";
 
 export default function BeforeStartWrite({ step, setStep }) {
   return (
-    <div>
+    <div className="before-start-write">
       <TitleBar
         name="작업실"
         intro="디테일이 생명입니다."
@@ -12,15 +12,31 @@ export default function BeforeStartWrite({ step, setStep }) {
         subintro2="stajio"
         img="pen"
       />
-      <div className="before-start-write">
+      <div className="before-start-write-content">
         <img
           src={process.env.PUBLIC_URL + "/img/circles_left.svg"}
           className="circle_left"
           alt="circle_left"
         ></img>
         <div className="before-start-write-box">
-          <span>내가 그리고 AI가 써주는 소설을 만들어볼까요?</span>
-          <button onClick={() => setStep(1)}>시작하기</button>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <div className="before-start-write-box-content">
+            <span>
+              내가 그리고
+              <br />
+              AI가 써주는 소설을
+              <br />
+              만들어볼까요?
+            </span>
+            <button onClick={() => setStep(1)}>시작하기</button>
+          </div>
         </div>
         <img
           src={process.env.PUBLIC_URL + "/img/circles_right.svg"}
