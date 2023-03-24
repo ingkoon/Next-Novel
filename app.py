8f82c46d97df1399fb9b841c128437643d3c5266
ejk9658 <ejk9658@naver.com>
2023-03-24 09:17:35 +0900

    add : 시간 출력 코드

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import asyncio
+import time
+
 from PIL import Image
 import io
 from fastapi import FastAPI, UploadFile, File, Form
@@ -7,9 +10,12 @@ from diffusion import creat_image
 from caption import inference_caption
 import googletrans
 import json
+import time
+import multiprocessing
 
 translator = googletrans.Translator()
 app = FastAPI()
+pool = multiprocessing.Pool(processes=3)
 
 
 
@@ -17,43 +23,56 @@ app = FastAPI()
 async def novel_start(images: List[UploadFile] = Form(...),
                        genre: str = Form(...)):
 
-    en_string = []
+    print(multiprocessing.cpu_count())
+
+    start = time.time()
+    image_bytes = []
     for image in images:
-        image_bytes = await image.read()
-        img = Image.open(io.BytesIO(image_bytes))
-        en_string.append(inference_caption(img))
-    question = "Act as a StoryTeller. Write an endless novel story in the genre of {} in 5 sentences based on {},{},{},{],{},{}.And write a sentence that summarizes this story in 3 sentences".format(genre,en_string[0],en_string[1],en_string[2],en_string[3],en_string[4],en_string[5])
+        image_bytes.append(await image.read())
+
+    print(time.time()-start)
+
+    # en_string = pool.map(inference_caption, image_bytes)
+    en_string = []
+    for i in image_bytes:
+        en_string.append(inference_caption(i))
+    # print(en_string)
+    question = "Act as a StoryTeller. Write an endless novel story in the genre of {} in 5 sentences based on {},{},{},{},{},{}.And write a sentence that summarizes this story in 3 sentences".format(genre,en_string[0],en_string[1],en_string[2],en_string[3],en_string[4],en_string[5])
     en_answer,new_history = chatbot(question,[])
     ko_answer = translator.translate(en_answer, dest="ko").text
+    print(time.time()-start)
 
     return {"korean_answer" : ko_answer,"dialog_history" : new_history}
 
 @app.post('/novel/question')
 async def novel_question(dialog_history:str=Form(...)):
     dialog_history = json.loads(dialog_history)
-    question =  "Ask me 3 questions. i wish the answers to those questions could be depicted in pictures"
+    question = "Ask me 3 questions I wish the answers to those questions could be depicted in pictures"
     en_answer, new_history = chatbot(question, dialog_history)
     ko_answer = translator.translate(en_answer, dest="ko").text
+    query = ko_answer.split("\n")
 
-    return {"english_answer" : ko_answer,"dialog_history" : new_history}
+    return {"query1" : query[0],"query2" : query[1],"query3" : query[2],"dialog_history" : new_history}
 
 @app.post('/novel/sequence')
 async def novel_sequence(image: UploadFile = Form(...),
                          previous_question:str=Form(...),
                          dialog_history:str=Form(...)):
 
+    start = time.time()
     dialog_history = json.loads(dialog_history)
 
     image_bytes = await image.read()
     img = Image.open(io.BytesIO(image_bytes))
     en_string = inference_caption(img)
 
-    question = "{}. the answer to th question is {}. Act as a Storyteller".format(previous_question,en_string)\
-               +"Write a 5 sentences novel without an ending to the story. and write a sentence that summarizes this story in 3 sentences"
+    question = "'{}' the answer to th question is '{}'. Act as a Storyteller.".format(previous_question,en_string)\
+               +"Write a 5 sentences novel without an ending to the story. And write a sentence that summarizes this story in 3 sentences"
 
 
     en_answer,new_history = chatbot(question,dialog_history)
     ko_answer = translator.translate(en_answer, dest="ko").text
+    print(time.time()-start)
 
     return {"korean_answer" : ko_answer,"dialog_history" : new_history}
 
@@ -65,7 +84,7 @@ async def novel_end(dialog_history:str=Form(...)):
 
     dialog_history = json.loads(dialog_history)
 
-    question = "act as a storyteller. write the ending of the story in 5 sentences"
+    question = "Act as a storyteller. Write the ending of the story in 5 sentences"
     en_answer, new_history = chatbot(question, dialog_history)
     ko_answer = translator.translate(en_answer, dest="ko").text
 
@@ -79,5 +98,5 @@ async def image(file: UploadFile = File(...)):
     return creat_image(open(file.filename,"rb"))[1]
 
 @app.get('/')
-def hello():
+async def hello():
     return "hello"
