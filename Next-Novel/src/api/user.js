ed0c97802c2e15320e0e3cdd7e259151061a502d
subinKim <subin3277@naver.com>
2023-03-30 16:46:40 +0900

    feature:mynovel

--- Next-Novel/src/api/user.js
+++ Next-Novel/src/api/user.js
@@ -1,6 +1,18 @@
 import { tokeninstance } from '../api/Interceptors';
 
+// 회원정보
 export async function user(){
   const res = await tokeninstance.get('user/')
+  return res
+}
+
+// 제작한 소설
+export async function getmynovel(){
+  const res = await tokeninstance.get('user/novel/')
+  return res
+}
+
+export async function getlikenovel(){
+  const res = await tokeninstance.get('user/liked-novel/')
   return res
 }
--- Next-Novel/src/components/common/Card.jsx
+++ Next-Novel/src/components/common/Card.jsx
@@ -14,33 +14,33 @@ function Card({props}){
 
   return (
     <div className={style.card}>
-      <div className={isHovering ? style.none : style.intro} style={ isHovering ? {'backgroundImage':`url(${props.cover_img})`}: {}} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+      <div className={isHovering ? style.none : style.intro} style={{'backgroundImage':`url(${props.cover_img})`}} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
         <div className={style.introment}>
           <div className={style.ment}>
-            {props.introduction}
+            {props && props.introduction}
           </div>
         </div>
         <img src={process.env.PUBLIC_URL+'/img/quote.png'} className={style.quote1} alt='quote1'></img>
         <img src={process.env.PUBLIC_URL+'/img/quote2.png'} className={style.quote2} alt='quote2'></img>
       </div>
-      <div className={isHovering ? style.intro : style.none } style={ isHovering ? {'backgroundImage':`url(${props.img})`}: {}} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
-        <div className={style.introment}>
+      <div className={isHovering ? style.intro : style.none } style={{'backgroundImage':`url(${props.cover_img})`}} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+        <div className={style.introment} >
           <div className={style.intro2}>
             <img src={process.env.PUBLIC_URL+'/icon/glasses.svg'} style={{margin:'auto 5px'}} alt='glasses'></img>
-            <span style={{margin:'0 5px'}}>{props.novel_stats.hit_count}</span>
+            <span style={{margin:'0 5px'}}>{props && props.novel_stats.hit_count}</span>
             <img src={process.env.PUBLIC_URL+'/icon/heart.svg'} style={{margin:'auto 5px'}} alt='heart'></img>
-            <span style={{margin:'0 5px'}}>{props.novel_stats.like_count}</span>
+            <span style={{margin:'0 5px'}}>{props && props.novel_stats.like_count}</span>
             <img src={process.env.PUBLIC_URL+'/icon/comment.svg'} style={{margin:'auto 5px'}} alt='comment'></img>
-            <span style={{margin:'0 5px'}}>{props.novel_stats.like_count}</span>
+            <span style={{margin:'0 5px'}}>{props && props.novel_stats.like_count}</span>
           </div>
           <img src={process.env.PUBLIC_URL+'/icon/trash.svg'} className={style.trash} alt='trash'></img>
         </div>
       </div>
       <div className={style.info}>
-        <div className={style.title}>{props.title}</div>
-        <div className={style.writer}>{props.author}</div>
+        <div className={style.title}>{props && props.title}</div>
+        <div className={style.writer}>{props && props.author}</div>
       </div>
-      <img src={props.cover_img} alt='bookimg'></img>
+      <img src={props && props.cover_img} alt='bookimg'></img>
     </div>
   )
 }
