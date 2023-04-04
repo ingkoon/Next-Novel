8023c635d621808c6bc703cfbb5f37cbaef70e97
subinKim <subin3277@naver.com>
2023-04-04 16:44:55 +0900

    feature:snakegame

--- Next-Novel/src/components/common/LoadingModal.jsx
+++ Next-Novel/src/components/common/LoadingModal.jsx
@@ -1,5 +1,7 @@
-import React from "react";
-import Modal from "react-modal";
+import React, { useEffect } from "react"
+import Modal from "react-modal"
+
+import Snakegame from "../game/Snakegame.jsx"
 
 export default function LoadingModal({ state }) {
   return (
@@ -11,18 +13,20 @@ export default function LoadingModal({ state }) {
             zIndex: "100",
           },
           content: {
-            width: "100vw",
-            height: "100vh",
+            width: "700px",
+            height: "500px",
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            inset: "0",
-            background: "none",
+            background: "#090909",
+            margin: "auto",
+            overflowX : 'hidden'
           },
         }}
       >
-        <img src={process.env.PUBLIC_URL + `/img/loading.gif`} alt="loading" />
+        {/* <img src={process.env.PUBLIC_URL + `/img/loading.gif`} alt="loading" /> */}
+        <Snakegame />
       </Modal>
     </>
-  );
+  )
 }
--- Next-Novel/src/components/novelwrite/WriteStep2.jsx
+++ Next-Novel/src/components/novelwrite/WriteStep2.jsx
@@ -16,57 +16,62 @@ export default function WriteStep2({ setStep, step, genreName }) {
   const [selected, setSelected] = useState(0);
   const [isShaking, setIsShaking] = useState(false);
 
+  const [isLoading, setIsLoading] = useState(false);
   const button = () => {
-    //그림 유효성 검사
-    for (let imageSrc of imageSrcs) {
-      if (!imageSrc) {
-        setIsShaking(true);
-        setTimeout(() => setIsShaking(false), 800); // 0.8초 후 클래스 제거
-        return;
-      }
-    }
+    setIsLoading(true)
+    setTimeout(() => setIsLoading(false), 800000); // 0.8초 후 클래스 제거
+  }
+  // const button = () => {
+  //   //그림 유효성 검사
+  //   for (let imageSrc of imageSrcs) {
+  //     if (!imageSrc) {
+  //       setIsShaking(true);
+  //       setTimeout(() => setIsShaking(false), 800); // 0.8초 후 클래스 제거
+  //       return;
+  //     }
+  //   }
 
-    const byteStrings = imageSrcs.map((dataUrl) =>
-      window.atob(dataUrl.split(",")[1])
-    );
-    const arrays = byteStrings.map((byteString) => {
-      let array = [];
-      for (let i = 0; i < byteString.length; i++) {
-        array.push(byteString.charCodeAt(i));
-      }
-      return array;
-    });
-    const myBlobs = arrays.map(
-      (array) => new Blob([new Uint8Array(array)], { type: "image/png" })
-    );
-    const files = myBlobs.map(
-      (myBlob, index) =>
-        new File([myBlob], "".concat("material", index, ".png"))
-    );
-    // console.log(files);
-    const formData = new FormData();
-    files.forEach((file) => formData.append("images", file));
-    formData.append("genre", genreName);
-    startNovel.mutate(formData, {
-      onSuccess: (res) => {
-        console.log(res);
-        //context 제어
-        setNovel({
-          ...novel,
-          genre: res.data.genre,
-          id: res.data.id,
-          step: res.data.step,
-          materials: res.data.materials,
-          story: res.data.story,
-        });
-        setStep(3);
-      },
-    });
-  };
+  //   const byteStrings = imageSrcs.map((dataUrl) =>
+  //     window.atob(dataUrl.split(",")[1])
+  //   );
+  //   const arrays = byteStrings.map((byteString) => {
+  //     let array = [];
+  //     for (let i = 0; i < byteString.length; i++) {
+  //       array.push(byteString.charCodeAt(i));
+  //     }
+  //     return array;
+  //   });
+  //   const myBlobs = arrays.map(
+  //     (array) => new Blob([new Uint8Array(array)], { type: "image/png" })
+  //   );
+  //   const files = myBlobs.map(
+  //     (myBlob, index) =>
+  //       new File([myBlob], "".concat("material", index, ".png"))
+  //   );
+  //   // console.log(files);
+  //   const formData = new FormData();
+  //   files.forEach((file) => formData.append("images", file));
+  //   formData.append("genre", genreName);
+  //   startNovel.mutate(formData, {
+  //     onSuccess: (res) => {
+  //       console.log(res);
+  //       //context 제어
+  //       setNovel({
+  //         ...novel,
+  //         genre: res.data.genre,
+  //         id: res.data.id,
+  //         step: res.data.step,
+  //         materials: res.data.materials,
+  //         story: res.data.story,
+  //       });
+  //       setStep(3);
+  //     },
+  //   });
+  // };
 
   return (
     <div className={style.container}>
-      <LoadingModal state={startNovel.isLoading} />
+      <LoadingModal state={isLoading} />
       <div className={style.component}>
         <Preview imageSrcs={imageSrcs} setSelected={setSelected} />
         <Canvas1
