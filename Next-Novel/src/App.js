339d3c85357db9c0e8ee70300b6abdf7b496fd53
소지현 <s688922@gmail.com>
2023-04-05 17:55:16 +0900

    feat : 작은화면 진입금지 화면 추가

--- Next-Novel/src/App.js
+++ Next-Novel/src/App.js
@@ -1,19 +1,22 @@
-import './App.css';
+import "./App.css";
 import { Outlet } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthContextProvider } from "./context/AuthContext";
 import AppBar from "./components/common/AppBar";
+import style from "./mobile.module.css";
 
 const queryClient = new QueryClient();
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <AuthContextProvider>
-        <AppBar />
-        <Outlet />
-      </AuthContextProvider>
-    </QueryClientProvider>
-
+    <>
+      <div className={style.overlay}>더 넓은 화면으로 조정하세요!</div>
+      <QueryClientProvider client={queryClient}>
+        <AuthContextProvider>
+          <AppBar />
+          <Outlet />
+        </AuthContextProvider>
+      </QueryClientProvider>
+    </>
   );
 }
 
