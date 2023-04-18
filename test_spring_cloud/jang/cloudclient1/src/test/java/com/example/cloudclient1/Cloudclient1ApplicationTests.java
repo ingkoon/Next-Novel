7860cd138b91874f9654851dfca3e260e9b28851
zlzlelzl <wkdwl1234@naver.com>
2023-04-18 11:22:05 +0900

    Feat: Spring Cloud test Added

---
+++ test_spring_cloud/jang/cloudclient1/src/test/java/com/example/cloudclient1/Cloudclient1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudclient1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cloudclient1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ test_spring_cloud/jang/cloudserver1/src/test/java/com/example/cloudserver1/Cloudserver1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudserver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cloudserver1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
