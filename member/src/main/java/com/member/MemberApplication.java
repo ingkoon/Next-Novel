8a5eac573b99edfc127c3411eae3c932351bb77f
ingkoon <dlswotmd@gmail.com>
2023-04-18 12:18:04 +0900

    Add : member domain 추가 및 서버간 통신 추가

---
+++ member/src/main/java/com/member/MemberApplication.java
@@ -0,0 +1,14 @@
+package com.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MemberApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MemberApplication.class, args);
+	}
+
+}
