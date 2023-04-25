043947a7bc058b1ba91eb80a721abdd913b5fc27
소지현 <s688922@gmail.com>
2023-04-25 09:38:25 +0900

    refact : NovelWrtie context jsx to tsx

---
+++ frontend/src/context/NovelContext.tsx
@@ -0,0 +1,85 @@
+import React, { createContext, useContext, useState } from "react";
+
+type Material = {
+  caption: string;
+  image: string;
+};
+type Question = {
+  index: number;
+  query: string;
+};
+type Cover = {
+  image:string,
+}
+
+type Novel = {
+  genre: string;
+  id: number | undefined;
+  step: number | undefined;
+  materials: Material[] | undefined;
+  newMaterials: Material[] | undefined;
+  story: string | undefined;
+  questions: Question[] | undefined;
+  selectedQuestion: number | undefined;
+  cover: Cover | undefined;
+};
+
+type NovelContextType = {
+  novel: Novel;
+  setNovel: React.Dispatch<React.SetStateAction<Novel>>;
+  step: number;
+  setStep: React.Dispatch<React.SetStateAction<number>>;
+  count: number;
+  setCount: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export const NovelContext = createContext<NovelContextType>({
+  novel: {
+    genre: "romance",
+    id: undefined,
+    step: undefined,
+    materials: undefined,
+    newMaterials: undefined,
+    story: undefined,
+    questions: undefined,
+    selectedQuestion: undefined,
+    cover: undefined,
+  },
+  setNovel: () => {},
+  step: 0,
+  setStep: () => {},
+  count: 0,
+  setCount: () => {},
+});
+
+export function NovelContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [novel, setNovel] = useState<Novel>({
+    genre: "romance",
+    id: undefined,
+    step: undefined,
+    materials: undefined,
+    newMaterials: undefined,
+    story: undefined,
+    questions: undefined,
+    selectedQuestion: undefined,
+    cover: undefined,
+  });
+  const [step, setStep] = useState<number>(0);
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <NovelContext.Provider
+      value={{ novel, setNovel, step, setStep, count, setCount }}
+    >
+      {children}
+    </NovelContext.Provider>
+  );
+}
+
+export function useNovelContext() {
+  return useContext<NovelContextType>(NovelContext);
+}
