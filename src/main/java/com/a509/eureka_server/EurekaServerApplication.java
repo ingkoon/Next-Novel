b75c43d59ba6dbe9cdebb99576d729e8383be561
ejk9658 <ejk9658@naver.com>
2023-04-26 09:26:54 +0900

    style : 패키지명 수정

--- src/main/java/com/a509/eureka_server/EurekaServerApplication.java
+++ src/main/java/com/a509/eureka_server/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.eureka_server;
+package com.a509.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/a509/eureka_server/EurekaServerApplicationTests.java
+++ src/test/java/com/a509/eureka_server/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.eureka_server;
+package com.a509.eureka_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
