1a1dbd07c868ef2f22ed9c275ae0c665dd0afb79
ejk9658 <ejk9658@naver.com>
2023-04-26 09:30:20 +0900

    style : 프로젝트명 및 패키지명 수정

--- src/main/java/com/a509/eureka_api_gateway/EurekaApiGatewayApplication.java
+++ src/main/java/com/a509/eureka_api_gateway/EurekaApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.example.eurekaapigateway;
+package com.a509.eureka_api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/a509/eureka_api_gateway/EurekaApiGatewayApplicationTests.java
+++ src/test/java/com/a509/eureka_api_gateway/EurekaApiGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.eurekaapigateway;
+package com.a509.eureka_api_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
