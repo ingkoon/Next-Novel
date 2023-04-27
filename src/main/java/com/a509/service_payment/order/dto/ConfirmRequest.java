80ac3fb0ce3bc692524610bbb9c652e1ef16d9c8
ingkoon <dlswotmd@gmail.com>
2023-04-27 11:15:26 +0900

    S08P31A509-341 Add : BootPayComponent 예외처리 로직 추가

--- src/main/java/com/a509/service_payment/order/dto/ConfirmRequest.java
+++ src/main/java/com/a509/service_payment/order/dto/ConfirmRequest.java
@@ -1,11 +1,21 @@
 package com.a509.service_payment.order.dto;
 
+import com.a509.service_payment.item.enums.ItemName;
+import com.a509.service_payment.order.domain.Order;
+import com.a509.service_payment.point.domain.Point;
 import lombok.Getter;
 
 @Getter
 public class ConfirmRequest {
-    private Long memberId;
-    private Long point;
+    private Long userId;
+    private ItemName itemName;
     private Long price;
     private String receiptId;
+
+    public Order toOrderEntity(){
+        return Order.builder()
+                .userId(userId)
+                .price(price)
+                .build();
+    }
 }
