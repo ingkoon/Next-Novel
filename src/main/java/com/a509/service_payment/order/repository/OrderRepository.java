ccd5b7db645b76378f82c668dee80bb9d530547e
ingkoon <ing97220@naver.com>
2023-04-29 17:20:35 +0900

    Add: OrderList repository 추가

--- src/main/java/com/a509/service_payment/order/repository/OrderRepository.java
+++ src/main/java/com/a509/service_payment/order/repository/OrderRepository.java
@@ -4,5 +4,9 @@ import com.a509.service_payment.order.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {}
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findAllByMemberId(Long memberId);
+}
