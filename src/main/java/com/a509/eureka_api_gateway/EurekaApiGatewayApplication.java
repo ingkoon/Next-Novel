b2455c39c6d6b064b127a8f08588c9c03b58e257
zlzlelzl <wkdwl1234@naver.com>
2023-05-03 15:07:38 +0900

    Update: Predicates Add Patterns

--- src/main/java/com/a509/eureka_api_gateway/EurekaApiGatewayApplication.java
+++ src/main/java/com/a509/eureka_api_gateway/EurekaApiGatewayApplication.java
@@ -3,13 +3,12 @@ package com.a509.eureka_api_gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class EurekaApiGatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApiGatewayApplication.class, args);
 	}
-
 }
