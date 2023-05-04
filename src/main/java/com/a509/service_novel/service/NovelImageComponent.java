2bc525c1c8ceb1124a6fe41f97ed83fcf8da2b6f
서울_15반_이명규 <si44440@naver.com>
2023-05-04 16:51:27 +0900

    fix : 파일 저장경로 임시 수정

--- src/main/java/com/a509/service_novel/service/NovelImageComponent.java
+++ src/main/java/com/a509/service_novel/service/NovelImageComponent.java
@@ -16,13 +16,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class NovelImageComponent {
 	// private final String path = "C:\\Users\\SSAFY\\Desktop\\imagelocation";
-	private final String path = "\\home\\ubuntu\\NextNovel\\NovelService\\images";
+	// private final String path = "\\home\\ubuntu\\NextNovel\\NovelService\\images";
+	private final String path = "";
 	// private final String path = "/app/data";
 	@Transactional
 	public void save(MultipartFile file, String UID) throws Exception{
+		String currentPaht = Paths.get("").toAbsolutePath().toString();
+		System.out.println(currentPaht);
 
 		// 저장할 파일 경로를 만듭니다.
 
-		file.transferTo(new File(path+"/"+UID+"_"+file.getOriginalFilename()));
+		file.transferTo(new File(currentPaht+"\\app\\data\\"+UID+"_"+file.getOriginalFilename()));
 	}
 }
