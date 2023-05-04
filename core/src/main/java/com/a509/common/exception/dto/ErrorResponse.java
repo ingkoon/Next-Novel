6ddd35ec712e8325ccd00b4cc2108fb7feaef867
ingkoon <dlswotmd@gmail.com>
2023-05-04 15:51:33 +0900

    Add : 예외처리 로직 이전 및 수정

---
+++ core/src/main/java/com/a509/common/exception/dto/ErrorResponse.java
@@ -0,0 +1,38 @@
+package com.a509.common.exception.dto;
+
+import com.a509.common.exception.enums.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private String code;
+    private String message;
+    private int status;
+    private String detail;
+
+    public ErrorResponse(final String message) {
+        this.message = message;
+    }
+    public ErrorResponse(ErrorCode code) {
+        this.code = code.getCode();
+        this.status = code.getStatus();
+        this.message = code.getMessage();
+        this.detail = code.getDetail();
+    }
+    public void setDetail(String detail){
+        this.detail = detail;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    public static ErrorResponse of(ErrorCode code){
+        return new ErrorResponse(code);
+    }
+}
