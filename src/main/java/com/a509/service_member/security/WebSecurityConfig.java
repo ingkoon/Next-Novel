5e03c8e72792f55299675fb37d34916debba7e07
ejk9658 <ejk9658@naver.com>
2023-05-04 16:13:12 +0900

    Update : google oauth 로그인 로직 수정

--- src/main/java/com/a509/service_member/security/WebSecurityConfig.java
+++ src/main/java/com/a509/service_member/security/WebSecurityConfig.java
@@ -37,7 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
                 .and()
                 // JwtAuthenticationFilter 를 UsernamePasswordAuthenticationFilter 전에 적용시킨다.
-//                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, stringRedisTemplate), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, stringRedisTemplate), UsernamePasswordAuthenticationFilter.class)
                 .oauth2Login()
                 .userInfoEndpoint()
                 .userService(customOAuth2UserService);
