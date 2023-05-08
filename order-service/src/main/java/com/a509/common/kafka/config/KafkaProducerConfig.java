5fbcb579189a4e6e93015846d9d6be2197a1c731
ingkoon <dlswotmd@gmail.com>
2023-05-08 17:28:20 +0900

    Add : kafka template 추가

---
+++ order-service/src/main/java/com/a509/common/kafka/config/KafkaProducerConfig.java
@@ -0,0 +1,31 @@
+package com.a509.common.kafka.config;
+
+import com.a509.dto.CreateRequestDto;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+    @Bean
+    public ProducerFactory<String, CreateRequestDto> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, CreateRequestDto> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
