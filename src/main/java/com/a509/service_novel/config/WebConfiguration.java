7296eddbb1599d2b75d05f936715e23a76e2ceaa
서울_15반_이명규 <si44440@naver.com>
2023-05-09 09:23:11 +0900

    fix : 이미지 경로수정 2

--- src/main/java/com/a509/service_novel/config/WebConfiguration.java
+++ src/main/java/com/a509/service_novel/config/WebConfiguration.java
@@ -7,10 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
-	private final String path = "/home/data";
+	// private final String path = "file:/C:/Users/SSAFY/Desktop/imagelocation/";
+	private final String path = "file:/home/data/";
+
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry){
 
-		registry.addResourceHandler("/img/**").addResourceLocations("file:/"+path);
+		registry.addResourceHandler("/img/**").addResourceLocations(path);
 	}
 }
