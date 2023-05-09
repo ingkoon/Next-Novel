fe437d9c3c2ded7561be517612179ab194d9ec44
서울_15반_이명규 <si44440@naver.com>
2023-05-09 09:05:39 +0900

    fix : image 경로 수정

--- src/main/java/com/a509/service_novel/config/WebConfiguration.java
+++ src/main/java/com/a509/service_novel/config/WebConfiguration.java
@@ -1,14 +1,19 @@
 package com.a509.service_novel.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.AllArgsConstructor;
+
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
+
+	private final String path = "/home/data";
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry){
 
-		registry.addResourceHandler("/img/**").addResourceLocations("file:/C:/Users/SSAFY/Desktop/imagelocation/");
+		registry.addResourceHandler("/img/**").addResourceLocations(path);
 	}
 }
