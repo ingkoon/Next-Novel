385e5e9ef219869c6551456a11796953862a2896
ingkoon <dlswotmd@gmail.com>
2023-05-10 06:12:39 +0900

    Add : 상태체크 requestdto 수정

--- core/src/main/java/com/a509/common/dto/order/request/IsCheckOrderRequest.java
+++ core/src/main/java/com/a509/common/dto/order/request/IsCheckOrderRequest.java
@@ -5,12 +5,35 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.io.Serializable;
+import java.util.Map;
 
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class IsCheckOrderRequest {
+public class IsCheckOrderRequest implements Serializable, Serializer {
     private Long orderId;
     private OrderStatus status;
+
+    @Override
+    public void configure(Map configs, boolean isKey) {
+    }
+
+    @Override
+    public byte[] serialize(String topic, Object data) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] serialize(String topic, Headers headers, Object data) {
+        return new byte[0];
+    }
+
+    @Override
+    public void close() {
+    }
 }
