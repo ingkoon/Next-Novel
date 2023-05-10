9f45b4a5e52fc65ad7cc76a63b05d514b46c3bd5
ejk9658 <ejk9658@naver.com>
2023-05-10 14:44:12 +0900

    Add : file upload 분리 시도

--- src/main/java/com/a509/service_member/ServiceMemberApplication.java
+++ src/main/java/com/a509/service_member/ServiceMemberApplication.java
@@ -1,15 +1,18 @@
 package com.a509.service_member;
 
+import com.a509.service_member.config.WebConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@Import(WebConfiguration.class)
 public class ServiceMemberApplication {
 	@Bean
 	public BCryptPasswordEncoder encodePwd() {
