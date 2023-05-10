03ffc07e3a3559bec7dd89206fa548e9c2e85999
ingkoon <ing97220@naver.com>
2023-05-11 01:02:55 +0900

    Add: Item 도메인 빌드파일 실행 시, 더미데이터 자동 생성 추가

---
+++ item-service/src/main/java/com/a509/dummy/inputItemData.java
@@ -0,0 +1,27 @@
+package com.a509.dummy;
+
+import com.a509.domain.Item;
+import com.a509.enums.Items;
+import com.a509.repostiory.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class inputItemData implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        addItem();
+    }
+    private final ItemRepository itemRepository;
+    private void addItem() {
+        for (Items items : Items.values()) {
+            Item tmp = Item.builder()
+                    .name(items)
+                    .build();
+            itemRepository.save(tmp);
+        }
+    }
+}
