a4f35beca78577e479eaca09826f384b2ff37733
ingkoon <dlswotmd@gmail.com>
2023-05-15 01:42:43 +0900

    Add : patch to put mapping

--- order-service/src/main/java/com/a509/common/kafka/config/KafkaProducerConfig.java
+++ order-service/src/main/java/com/a509/common/kafka/config/KafkaProducerConfig.java
@@ -47,7 +47,7 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, CreateOrderItemRequestDto> createOrderItemFactory() {
         configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaUrl);
         configProps.put(ProducerConfig.CONNECTIONS_MAX_IDLE_MS_CONFIG, 5000); // 5초로 설정
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
