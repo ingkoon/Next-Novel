3d92dc55a0014a6b0102047b221503440f7fcaf3
ejk9658 <ejk9658@naver.com>
2023-05-14 17:57:26 +0900

    Update : google login 닉네임 저장 형태 변경

--- src/main/java/com/a509/service_member/service/CustomOAuth2UserService.java
+++ src/main/java/com/a509/service_member/service/CustomOAuth2UserService.java
@@ -33,30 +33,43 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         // 회원가입을 강제로 진행
         OAuth2UserInfo oAuth2UserInfo = null;
         if (userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+            // 구글 로그인 요청
             System.out.println("구글 로그인 요청");
             oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
         }
 
-        String provider = oAuth2UserInfo.getProvider();
-        String providerId = oAuth2UserInfo.getProviderId();
-        String email = provider + "_" + providerId;
-        Optional<Member> optionalMember = memberRepository.findByEmail(email);
-        Member member = null;
+        String provider = oAuth2UserInfo.getProvider(); // ex)google
+        String providerId = oAuth2UserInfo.getProviderId(); // ex)134543523124
+        String providerEmail = provider + "_" + providerId;
+        // 해당 이메일로 이미 가입 되어있는지 확인함
+        Optional<Member> optionalMember = memberRepository.findByEmail(providerEmail);
+
+        Member member;
         if (optionalMember.isEmpty()) {
+            // 해당 닉네임으로 이미 가입 되어있는지 확인함
+            String nickname = oAuth2UserInfo.getNickName();
+            int num = 0;
+            while(memberRepository.existsByNickName(nickname)) {
+                nickname = oAuth2UserInfo.getNickName()+"_"+(++num);    // ex)닉네임_0
+            }
+
+            // 회원가입 진행
             member = Member
                     .builder()
-                    .email(email)
-                    .password(bCryptPasswordEncoder.encode(providerId))
-                    .nickName(oAuth2UserInfo.getEmail())
+                    .email(providerEmail)
+                    .password(bCryptPasswordEncoder.encode("딘추"))
+                    .nickName(nickname)
                     .profileImage(oAuth2UserInfo.getProfileImage())
                     .provider(provider)
                     .providerId(providerId)
                     .build();
-            memberRepository.save(member);
+            member = memberRepository.save(member);
+        } else {
+            member =optionalMember.get();
         }
 
-        System.out.println(new CustomUserDetails(optionalMember.orElse(member), oAuth2User.getAttributes()));
-        return new CustomUserDetails(optionalMember.orElse(member), oAuth2User.getAttributes());
+        System.out.println(member.toString());
+        return new CustomUserDetails(member, oAuth2User.getAttributes());
     }
 
 //    @Override
