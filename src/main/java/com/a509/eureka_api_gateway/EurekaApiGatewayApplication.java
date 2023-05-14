1dd60d1f6f782f13842a2a8be6467e57230e4d16
LeeMyeonggyu <si44440@naver.com>
2023-05-14 10:10:33 +0900

    update : csrf 설정 비활성화

--- src/main/java/com/a509/eureka_api_gateway/EurekaApiGatewayApplication.java
+++ src/main/java/com/a509/eureka_api_gateway/EurekaApiGatewayApplication.java
@@ -3,7 +3,11 @@ package com.a509.eureka_api_gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,4 +15,10 @@ public class EurekaApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApiGatewayApplication.class, args);
 	}
+
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity security){
+		return security.csrf().disable().build();
+	}
+
 }
