95174ee7e96d5eae6b9cbff05a54a3acebef3806
leemg <si44440@naver.com>
2023-05-14 09:16:00 +0900

    init commit

--- src/main/java/com/a509/eureka_api_gateway/EurekaApiGatewayApplication.java
+++ src/main/java/com/a509/eureka_api_gateway/EurekaApiGatewayApplication.java
@@ -3,6 +3,7 @@ package com.a509.eureka_api_gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
@@ -11,4 +12,5 @@ public class EurekaApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApiGatewayApplication.class, args);
 	}
+
 }
