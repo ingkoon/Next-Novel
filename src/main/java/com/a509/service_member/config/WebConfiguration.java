14fa976a69e93c896164c8f880fe56243313e2de
ejk9658 <ejk9658@naver.com>
2023-05-15 14:50:56 +0900

    Update : JWT 토큰 생성 시 닉네임 없음

--- src/main/java/com/a509/service_member/config/WebConfiguration.java
+++ src/main/java/com/a509/service_member/config/WebConfiguration.java
@@ -7,8 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
-    // private final String path = "file:/C:/Users/SSAFY/Desktop/imagelocation/";
-    private final String path = "file:/home/data/";
+     private final String path = "file:/C:/Users/SSAFY/Desktop/imagelocation/";
+//    private final String path = "file:/home/data/";
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
