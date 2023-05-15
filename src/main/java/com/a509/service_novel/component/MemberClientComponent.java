def9dc3c8e4802946a7285824321fc644a27bd79
서울_15반_이명규 <si44440@naver.com>
2023-05-15 16:27:19 +0900

    fix : something

--- src/main/java/com/a509/service_novel/component/MemberClientComponent.java
+++ src/main/java/com/a509/service_novel/component/MemberClientComponent.java
@@ -1,11 +1,18 @@
 package com.a509.service_novel.component;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import com.a509.service_novel.dto.MemberMyPageResponseDto;
 
 @FeignClient(name = "SERVICE-MEMBER", url = "***REMOVED***:8000/member")
 public interface MemberClientComponent {
 	@GetMapping("/myPage/image/{nick-name}")
 	String findMyPageImage(@PathVariable("nick-name") String nickName);
+
+	@GetMapping("/myPage")
+	ResponseEntity<MemberMyPageResponseDto> findMyPage(@RequestHeader("Authorization") final String token);
 }
