3f3e2babf849c60044668010258b0c392b4f8864
ejk9658 <ejk9658@naver.com>
2023-05-15 13:54:31 +0900

    Modify : access token 1분 test

--- src/main/java/com/a509/service_member/jwt/JwtTokenProvider.java
+++ src/main/java/com/a509/service_member/jwt/JwtTokenProvider.java
@@ -33,8 +33,8 @@ public class JwtTokenProvider {
     private static final String NAME_KEY = "name";
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "Bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 30 * 60 * 1000L;              // 30분
-//    private static final long ACCESS_TOKEN_EXPIRE_TIME = 3 * 1000L;              // 3초
+//    private static final long ACCESS_TOKEN_EXPIRE_TIME = 30 * 60 * 1000L;              // 30분
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 60 * 1000L;              // 1분
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 7 * 24 * 60 * 60 * 1000L;    // 7일
 //    private static final long REFRESH_TOKEN_EXPIRE_TIME = 60 * 1000L;    // 1분
     private final Key key;
