77a8b46835b3b3db40456a62271888f1c4be48fb
서울_5반_이명규 <si44440@naver.com>
2023-05-15 17:45:40 +0900

    Update src/main/java/com/a509/service_member/controller/MemberController.java

--- src/main/java/com/a509/service_member/controller/MemberController.java
+++ src/main/java/com/a509/service_member/controller/MemberController.java
@@ -12,6 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 
 @Slf4j
 @RestController
