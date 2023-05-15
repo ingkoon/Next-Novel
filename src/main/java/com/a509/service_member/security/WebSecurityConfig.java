2a10abc429a355ddb4dc65a933a1e0bec4878e23
ejk9658 <ejk9658@naver.com>
2023-05-15 17:12:50 +0900

    Update : oauth2 로그인 후 프론트 화면 리디렉션

--- src/main/java/com/a509/service_member/security/WebSecurityConfig.java
+++ src/main/java/com/a509/service_member/security/WebSecurityConfig.java
@@ -1,24 +1,17 @@
 package com.a509.service_member.security;
 
-//import com.a509.service_member.jwt.JwtAuthenticationFilter;
-import com.a509.service_member.jwt.JwtTokenProvider;
-//import com.a509.service_member.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final JwtTokenProvider jwtTokenProvider;
-    private final StringRedisTemplate stringRedisTemplate;
-//    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,18 +24,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/member/**").permitAll()
                 .antMatchers("/user/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
                 .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
-                .anyRequest().permitAll();
-//                .and()
-//                .oauth2Login()
-//                .userInfoEndpoint(userInfo -> {
-//                    userInfo.userService(customOAuth2UserService);
-//                })
-//                .and()
-//                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, stringRedisTemplate), UsernamePasswordAuthenticationFilter.class);
+                .anyRequest().permitAll()
+                .and()
+                .oauth2Login()
+                .successHandler(successHandler());
     }
 
-//    @Bean
-//    public BCryptPasswordEncoder encodePwd() {
-//        return new BCryptPasswordEncoder();
-//    }
+    private AuthenticationSuccessHandler successHandler() {
+        return (request, response, authentication) -> {
+            // 로그인 성공 후 토큰을 생성하거나 필요한 정보를 추출합니다.
+            // 프론트엔드 서버로 리디렉션합니다.
+            response.sendRedirect("https://***REMOVED***");
+        };
+    }
+
 }
