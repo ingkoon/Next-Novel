b83ad99da25e080b8f39d678bbbae7e2c0d7bf77
소지현 <s688922@gmail.com>
2023-05-17 16:22:07 +0900

    refact : Access token 관련 주석

--- frontend/src/api/Interceptors.ts
+++ frontend/src/api/Interceptors.ts
@@ -1,12 +1,8 @@
 import axios from "axios";
-import { useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../context/AuthContext";
 
 axios.defaults.withCredentials = true;
 const instance = axios.create({
   baseURL: process.env.REACT_APP_DATA_API,
-  // baseURL : 'https://nextnovel.site/api/'
 });
 
 const tokeninstance = axios.create({
@@ -33,31 +29,13 @@ instance.interceptors.response.use(
   }
 );
 
-// function TokenInterceptor({children}) {
-
-//   useEffect(() => {
-//     console.log("@@@")
-//     const resInterceptor = response => {
-//       return response
-//     }
-
-//     const errInterceptor = error => {
-//       return Promise.reject(error)
-//     }
-
-//     const interceptor = tokeninstance.interceptors.response.use(resInterceptor, errInterceptor)
-//     return () => tokeninstance.interceptors.response.eject(interceptor)
-//   }, [])
-//   return children
-// }
-
 // 토큰 필요한 api
 tokeninstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("access_token");
-    // const token = user.access_token
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
+      // console.log("보내는 토큰 : ", token);
     }
     return config;
   },
@@ -72,10 +50,20 @@ tokeninstance.interceptors.response.use(
     if (response.headers["authorization"]) {
       const access_token = response.headers.authorization.split(" ")[1];
       localStorage.setItem("access_token", access_token);
+      // console.log("받는 토큰 : ", access_token);
     }
     return response;
   },
   (error) => {
+    // const { config, response } = error;
+    // const originalRequest = config;
+
+    // if (response.status === 401) {
+    //   return new Promise((resolve) => {
+    //     setTimeout(() => resolve(tokeninstance(originalRequest)), 1000);
+    //   });
+    // }
+
     return Promise.reject(error);
   }
 );
