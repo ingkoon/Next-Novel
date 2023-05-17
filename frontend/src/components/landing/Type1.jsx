08ee2515d4d99c40f1499164667ab3536ede1171
ingkoon <ing97220@naver.com>
2023-05-18 00:37:02 +0900

    Add : landing페이지 에러 제거

--- frontend/src/components/landing/Type1.jsx
+++ frontend/src/components/landing/Type1.jsx
@@ -1,48 +1,16 @@
 import style from './Type1.module.css';
-import { useEffect, useRef } from 'react';
+import TypeIt from "typeit-react";
 
 export default function Type1() {
-  const typeitRef = useRef(null);
-
-  useEffect(() => {
-    const startTypingAnimation = () => {
-      const instance = new window.TypeIt(typeitRef.current, {
-        strings: ['이런 상상 해본적 있나요?'],
-        speed: 50,
-        loop: false,
-      });
-
-      instance.go();
-    };
-
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            startTypingAnimation();
-            observer.unobserve(entry.target);
-          }
-        });
-      },
-      {
-        threshold: 1.0, // The observer callback will be called when the element is 100% visible
-      }
-    );
-
-    if (typeitRef.current) {
-      observer.observe(typeitRef.current);
-    }
-
-    return () => {
-      if (typeitRef.current) {
-        observer.unobserve(typeitRef.current);
-      }
-    };
-  }, []);
-
   return (
     <div className={style.wrapper}>
-      <div ref={typeitRef} className={style.aaa}></div>
+      <TypeIt className={style.aaa}
+          options={{
+              strings: ["이런 상상 해본적 있나요?"],
+              speed: 50,
+              waitUntilVisible: true,
+          }}
+      />
     </div>
   );
 }
