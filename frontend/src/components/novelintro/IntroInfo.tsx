a2a0eb179d3c2ca345ba7ebff4a205c137dce233
verandaaa <s688922@gmail.com>
2023-05-18 23:23:10 +0900

    refact : Intro novelId 불러오기 location -> params 변경

--- frontend/src/components/novelintro/IntroInfo.tsx
+++ frontend/src/components/novelintro/IntroInfo.tsx
@@ -3,12 +3,12 @@ import Thumbnail from "./Thumbnail";
 import BookInfo from "./BookInfo";
 import Comments from "./Comments";
 import { getintro } from "../../api/novel";
-import { useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { NovelInfoType } from "../../types/novel";
 
 export default function IntroInfo() {
-  const location = useLocation();
-  const novelId = location.state.novelId;
+  const { id } = useParams();
+  const novelId = Number(id)!;
   const [novelInfo, setNovelInfo] = useState<NovelInfoType | undefined>();
   const memberId = localStorage.getItem("memberId")
     ? Number(localStorage.getItem("memberId"))
