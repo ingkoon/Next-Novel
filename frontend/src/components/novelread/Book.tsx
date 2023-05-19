f9d5c86c428947c9076b2d19886f83c6b9ae409f
서철원 <96charles@naver.com>
2023-05-19 10:34:42 +0900

    refact: novelread bug fix

--- frontend/src/components/novelread/Book.tsx
+++ frontend/src/components/novelread/Book.tsx
@@ -53,6 +53,7 @@ export default function Book() {
   const [novelid, setNovelid] = useState(Nid);
   const [novelinfo, setNovelinfo] = useState<Ninfo>();
   const [novelMat, setNovelMat] = useState<NMat>([]);
+  const [novelStart, setStart] = useState("");
   const [novelContent, setNovelContent] = useState<Ncontent>();
   const [lastPage, setLastPage] = useState("");
   const [rerender, setRerender] = useState("");
@@ -79,6 +80,7 @@ export default function Book() {
         title: data.data.title,
       });
       setNovelMat(data.data.startImages);
+      setStart(data.data.startContent);
       setNovelContent(
         data.data.contents
       );
@@ -195,12 +197,18 @@ export default function Book() {
                 <div className={style.fbar} />
               </div>
             </div>
+            <div className={style.page} ref={page_ref}>
+              <Materials mat={novelMat} />
+            </div>
+            <div className={style.page} ref={page_ref}>
+              <div className={style.text}>{novelStart}</div>
+            </div>
             {novelContent?.map((item, index) => {
               return (
                 <>
                   <div className={style.page} ref={page_ref}>
-                    {index === 0 && <Materials mat={novelMat} />}
-                    {index > 0 && <Qna qna={item} index={index} />}
+                    {/* {index === 0 && <Materials mat={novelMat} />} */}
+                    {<Qna qna={item} index={index} />}
                   </div>
                   <div className={style.page} ref={page_ref}>
                     <span className={style.text}>{item.content}</span>
